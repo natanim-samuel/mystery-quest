d589f2f417a2146bd340bbb884055ffcd49c9691
natanim-samuel <ntnmsamuel@gmail.com>
2026-09-18 20:06:44 +0300

    section title

---
+++ lib/widgets/section_title.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class SectionTitle extends StatelessWidget {
+  final String title;
+  final String? actionText;
+  final VoidCallback? onAction;
+
+  const SectionTitle({
+    super.key,
+    required this.title,
+    this.actionText,
+    this.onAction,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 21,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        if (actionText != null)
+          TextButton(
+            onPressed: onAction,
+            child: Text(actionText!),
+          ),
+      ],
+    );
+  }
+}
